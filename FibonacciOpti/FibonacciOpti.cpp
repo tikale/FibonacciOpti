1f690288bd181770ea2ce92de223d1fc5b3adbfe
tikale <lekawa77@gmail.com>
2021-11-13 16:15:58 +0100

    optimization

--- FibonacciOpti/FibonacciOpti.cpp
+++ FibonacciOpti/FibonacciOpti.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-unsigned int  fib(unsigned int);
+unsigned int  fib(unsigned int, unsigned int *tab);
 
 int main()
 {
@@ -10,14 +10,22 @@ int main()
 	{
 		cout << "Witam. Aby wyjsc wpisz 0.\nPodaj numer elementu ciagu: ";
 		cin >> elementy;
-		if (elementy > 40)
-			cout << "Za duzo, max 40\n";
-		else if (!elementy)
+		unsigned int * tablica = new unsigned int[elementy+1];
+		for (unsigned int ii = 0; ii <= elementy; ii++)
+		{
+			tablica[ii] = 0;
+		}
+
+		if (!elementy)
+		{
+			delete [] tablica;
 			break;
+		}
 		else
 		{
 			cout << "\nA oto wartosc elementu ciagu Fibonacciego: ";
-			cout << fib(elementy);
+			cout << fib(elementy, tablica);
+			delete [] tablica;
 		}
 		cout << "\n\n";
 	}
@@ -25,8 +33,11 @@ int main()
 	return 0;
 }
 
-unsigned int fib(unsigned int n) {
+unsigned int fib(unsigned int n, unsigned int *tab) {
+	if (tab[n] > 0)
+		return tab[n];
 	if (n == 0) return 0;
 	if (n == 1) return 1;
-	return fib(n - 1) + fib(n - 2);
+	tab[n] = fib(n - 1, tab) + fib(n - 2, tab);
+	return tab[n];
 }
